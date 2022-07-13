e92bd6bc0eb1fa446e23f1dac0ab0fb8e0ddd27f
Murat YILMAZ <muratyilmaz1634@gmail.com>
2022-07-14 01:15:06 +0300

    Configure eventSlice.js for async operations (redux)

---
+++ src/features/eventSlice.js
@@ -0,0 +1,40 @@
+import { allEvents } from "../config/api";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  items: [],
+  isLoading: [],
+  searchQuery: "",
+};
+
+export const getEvents = createAsyncThunk("events/getEvents", async () => {
+  const res = await axios(allEvents());
+  return res.data;
+});
+
+const eventSlice = createSlice({
+  name: "event",
+  initialState,
+  reducers: {
+    setSearchQuery: (state, action) => {
+      state.searchQuery = action.payload;
+    },
+  },
+  extraReducers: {
+    // For fetching events
+    [getEvents.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getEvents.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.items = action.payload;
+    },
+    [getEvents.rejected]: (state) => {
+      state.isLoading = false;
+    },
+  },
+});
+
+export const { setSearchQuery } = eventSlice.actions;
+export default eventSlice.reducer;
